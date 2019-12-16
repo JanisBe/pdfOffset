704939bfbbce8592e7352e7c4e09f5e64b852919
janis.blatsios <external.janis.blatsios@kuehne-nagel.com>
2019-12-16 13:50:44 +0100

    dodany porządnie md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pdfOffset
-creating offset for two side printing of pdfs
+Creating offset for two side printing of pdfs<br>
+You can download it [here](https://github.com/JanisBe/pdfOffset/blob/master/pdfOffset.jar).<br>
+Run as
+```console
+java -jar pdfOffset.jar
+```
+Click `Test pdf file` button to print test file. <br>
+**Dont forget to print it both-sides!**<br>
+Measure distance between dots as described on the pdf (you can make holes on one side using a pen) and input the values to the program.
+-----
+<h2>Tworzenie rozsuniecia miedzy stronami w pdfach przy druku dwustronnym</h2>
+Mozna pobrac [tutaj](https://github.com/JanisBe/pdfOffset/blob/master/pdfOffset.jar).
+<br>Uruchom jako
+```console
+java -jar pdfOffset.jar
+```
+ Kliknij przycisk `Test pdf file` aby wydrukowac testowego pdfa. <br>
+ **Nie zapomnij wydrukować go dwustronnie!**<br>
+ Zmmierz odległość pomiędzy kropkami (można zrobić dziurki np długopiesem) tak, jak opisano na pdfie i wpisz zmierzone wartości do programu.
--- src/main/java/janis/service/NewPdf.java
+++ src/main/java/janis/service/NewPdf.java
@@ -1,10 +1,15 @@
 package janis.service;
 
+import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.kernel.colors.DeviceCmyk;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 
 import java.io.FileNotFoundException;
 
@@ -15,7 +20,20 @@ public class NewPdf {
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(FILE));
         Document doc = new Document(pdfDoc);
         drawCircleOnNewPage(pdfDoc);
+        PdfCanvas pdfCanvas = new PdfCanvas(pdfDoc.getFirstPage());
+        Rectangle rectangle = new Rectangle(250, 420, 200, 15);
+        Rectangle rectangle2 = new Rectangle(150, 100, 400, 200);
+        Canvas canvas = new Canvas(pdfCanvas, pdfDoc, rectangle);
+        Canvas noteCanvas = new Canvas(pdfCanvas, pdfDoc, rectangle2);
+        canvas.add(new Paragraph("Pierwsza strona / First page"));
+        noteCanvas.add(new Paragraph("Measure distance in x and y between dots on first page and put it in."));
+        noteCanvas.add(new Paragraph("Zmierz odleglosc po x i y miedzy punktami na pierwszej i drugiej stronie"));
+        noteCanvas.add(new Paragraph("Jezeli punkt na 2 stronie jest wyzej to wartosc X jest dodatnia"));
+        noteCanvas.add(new Paragraph("Jezeli punkt na 2 stronie jest na prawo to wartosc Y jest dodatnia"));
         drawCircleOnNewPage(pdfDoc);
+        pdfCanvas = new PdfCanvas(pdfDoc.getLastPage());
+        canvas = new Canvas(pdfCanvas, pdfDoc, rectangle);
+        canvas.add(new Paragraph("Druga strona / Second page"));
         doc.close();
         pdfDoc.close();
         return FILE;
@@ -29,5 +47,8 @@ public class NewPdf {
         canvas.circle(100, 750, 2);
         canvas.circle(500, 750, 2);
         canvas.fill();
+        Rectangle rectangle = new Rectangle(240, 415, 180, 20);
+        canvas.rectangle(rectangle);
+        canvas.stroke();
     }
 }
